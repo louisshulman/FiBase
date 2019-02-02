c1e191c3dfd098bdb694c7676e7d2fd070ce51a4
JoshChima <45519648+JoshChima@users.noreply.github.com>
2019-02-02 14:19:01 -0600

    uploaded balance sheet

---
+++ Logic/Balance_Sheet.py
@@ -0,0 +1,13 @@
+cash = input()
+accounts_receivable = input()
+inventory = input()
+Prepaid_expenses = input()
+landcst = input()
+buildingscst = input()
+equipmentcst = input
+Fixed_assets = landcst + buildingscst + equipmentcst
+
+
+def Assets(cash):
+def Liabilities()
+def Equity()
