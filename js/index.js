3e965049f8c9d4cc22c16c13e551a5d8a5131f06
louisshulman <louisjs46@gmail.com>
2019-02-02 15:42:43 -0800

    Updates to CSS, html, js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // alert("This works!");
+
+function formSubmitted(){
+    var xx = document.getElementById("x").value;
+    var yy = document.getElementById("y").value;
+    var zz = document.getElementById("z").value;
+    var ww = document.getElementById("w").value;
+    console.log(xx + " " + yy + " " + zz + " " + ww + " ");
+    
+}
+
+document.getElementById("myForm").onsubmit = function(){formSubmitted()};
+
+
