3e16042ee537e1dc83d1703bf9a3281d2a76d5a1
louisshulman <louisjs46@gmail.com>
2019-02-02 12:49:43 -0800

    not sure what i did

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -38,14 +38,4 @@ def create_app(test_config=None):
     app.register_blueprint(blog.bp)
     app.add_url_rule('/', endpoint='index')
 
-@bp.route('/')
-def index():
-    db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
-    return render_template('blog/index.html', posts=posts)
-
     return app
