7165043c01bdaba638029d4b53971d6b7ea2eb8d
louisshulman <louisjs46@gmail.com>
2019-02-02 21:51:22 -0600

    some updates overdue

--- js/index.js
+++ js/index.js
@@ -1,26 +1,33 @@
-// alert("This works!");
+// Thanks David and ACM Website - https://github.com/UA-ACM-Student-Chapter/UA-ACM-Student-Chapter.github.io/blob/master/js/acm-1.0.1.js
+var getFormData = function($form) {
+    var unindexed_array = $form.serializeArray();
+    var indexed_array = {};
+    $.map(unindexed_array, function(n, i) {
+        indexed_array[n["name"]] = n["value"];
+    });
+    return indexed_array;
+}
 
-// function formSubmitted(){
-//     var cv19 = document.getElementById("2019").value;
-//     var cv18 = document.getElementById("2018").value;
-//     var cv17 = document.getElementById("2017").value;
-//     var cv16 = document.getElementById("2016").value;
-//     var cv15 = document.getElementById("2016").value;
-//     console.log(cv15 + " " + cv16 + " " + cv17 + " " + cv18 + " " + cv19 + "");
-    
-// }
-
-// document.getElementById("cashForm").onsubmit = function(){formSubmitted()};
-
-
-
-
+// Thanks David and ACM Website - https://github.com/UA-ACM-Student-Chapter/UA-ACM-Student-Chapter.github.io/blob/master/js/acm-1.0.1.js
 $("#cashSubmit").click(function(e){
     var $forma = $("#cashForm");
-    // Thanks David and ACM Website - https://github.com/UA-ACM-Student-Chapter/UA-ACM-Student-Chapter.github.io/blob/master/js/acm-1.0.1.js
-    var $forma = $("#cashForm");
-    var data = getFormData($forma);
-    var jsonnn = JSON.stringify(data);
-    alert(jsonnn);
+    var formdata = getFormData($forma);
+    $(#"cashSubmit").fade();
+    // var jsonnn = JSON.stringify(formdata);
+    $.post({
+        url: "http://1270.0.0.1:5000/npv",
+        data: JSON.stringify(formdata),
+        contentType: "application/json",
+        dataType: "json",
+        success: function(response) {
+            if (response["success"] == true) {
+                console.log("Communication!")
+            }
+            else {
+                alert(response['errorMessage'])
+            }
+            $(#"cashSubmit").show();
+        }
+    })
     e.preventDefault();
 });
