95fef96aefdb2f056797486451ee9212a3222ba9
JoshChima <45519648+JoshChima@users.noreply.github.com>
2019-02-02 22:58:48 -0600

    Ball Rolling

--- Logic/Newnpv.py
+++ Logic/Newnpv.py
@@ -4,9 +4,13 @@ def npv(a, b, c, d, e, i):
     for num in range(len(lst)):
         dci = lst[num] / (1 + i)**(num)
         dcilst.append(dci)
-    NPV = sum(dcilst)
-    print(dcilst)
-    print(NPV)
-
+    NPV = round((sum(dcilst)), 2)
+    print("NPV = " + str(NPV))
+    if NPV >= 0:
+        print("Project made the required annual rate of return of " + str(i) + "%")
+        print("Project go ahead approved")
+    else:
+        print("Project did not make the required annual rate of return of " + str(i) + "%")
+        print("Project denied")
 
 npv(-100, 50, 40, 30, 20, .04)
