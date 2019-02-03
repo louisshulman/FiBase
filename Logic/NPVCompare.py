a5185043f129c83d24c668eaef042b0f3baa6dcd
JoshChima <45519648+JoshChima@users.noreply.github.com>
2019-02-03 10:47:01 -0600

    Final

--- Logic/NPVCompare.py
+++ Logic/NPVCompare.py
@@ -1,10 +1,15 @@
-nlst = [-100, 500, 200, 600]
+nlst = [-20000, 7000, 5000, 8000, 9000]
 def npv(i):
     lst = nlst
     dcilst = []
     for num in range(len(lst)):
-        dci = lst[num] / (1 + i)**(num)
+        if num == 0:
+            dci = round((lst[0]), 2 )
+        else:
+            dci = round(lst[num] / ((1 + i)**(num)), 2)
+        print(dci)
         dcilst.append(dci)
+    print(dcilst)
     NPV = round((sum(dcilst)), 2)
     print("NPV = " + str(NPV))
     if NPV >= 0:
@@ -14,5 +19,4 @@ def npv(i):
         print("Project did not make the required annual rate of return of " + str(i) + "%")
         print("Project denied")
 
-npv(.04)
-
+print(npv(0.08))
