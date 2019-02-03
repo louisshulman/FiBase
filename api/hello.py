24455df455aeb094be0617919f6999d6283f3bfd
louisshulman <louisjs46@gmail.com>
2019-02-02 23:32:42 -0600

    IT FOOKING WORKS

--- api/hello.py
+++ api/hello.py
@@ -1,19 +1,24 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS 
 
 
 app = Flask(__name__)
+CORS(app)
 
 
-def computeAvg(a,b,c,d,e):
+def computeAvg(a,b,c,d,e,f):
     return ((a+b+c+d+e)/5)
 
 @app.route('/npv', methods = ['POST','GET'])
 def npvMult():
+    print("something")
     r = request.get_json()
-    x19 = r["a"]
-    x18 = r["b"]
-    x17 = r["c"]
-    x16 = r["d"]
-    x15 = r["e"]
-    avg = computeAvg(x19, x18, x17, x16, x15)
-    return jsonify(NPV=str(avg))
+    x19 = int(r["x2019"])
+    print(x19)
+    x18 = int(r["x2018"])
+    x17 = int(r["x2017"])
+    x16 = int(r["x2016"])
+    x15 = int(r["x2015"])
+    xDR = int(r["xdr"])
+    avg = computeAvg(x19, x18, x17, x16, x15, xDR)
+    return jsonify(npv=avg,success=True)
